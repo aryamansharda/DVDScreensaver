8a164a37f4ce0a3680d0c4320de93d83c54b722b
aryamansharda <asharda@turo.com>
2024-03-22 20:30:50 -0700

    Initial Commit

---
+++ DVDScreensaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DVDScreensaver
+//
+//  Created by Aryaman Sharda on 3/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DVDScreensaver/DVDScreensaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVDScreensaverApp.swift
+//  DVDScreensaver
+//
+//  Created by Aryaman Sharda on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DVDScreensaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
